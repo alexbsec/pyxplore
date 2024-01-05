d4e60a03555ed5d65d8e6ee81049c45738cad698
unknown <j.a.c.buschinelli@gmail.com>
2024-01-05 11:16:44 -0300

    fixed color output, custom wl and more

--- src/xrequests.py
+++ src/xrequests.py
@@ -2,21 +2,7 @@ import requests
 from . import colorizer
 import time
 import re
-
-def coprint(text):
-    co = colorizer.ColorOutput()
-    match text:
-        case _ if re.match(r"^2\d{2}$", text):
-            co.green.print(text)
-        case _ if re.match(r"^3\d{2}$", text):
-            co.blue.print(text)
-        case _ if re.match(r"^4\d{2}$", text):
-            co.red.print(text)
-        case _ if re.match(r"^5\d{2}$", text):
-            co.red.print(text)
-        case _:
-            co.white.print(text)  # Default case if none of the above patterns match
-
+import json
 
 class XploreRequest(requests.Session):
     WORDLISTS = {
@@ -31,6 +17,8 @@ class XploreRequest(requests.Session):
                         'small': 'https://wordlists-cdn.assetnote.io/data/manual/aspx_lowercase.txt'
                     }
             }
+    
+    STATUS_DICT = {}
 
     def __init__(self, url, gcode, typ, output=None, size='small', delay=0, use_https=True, wl=None):
         # check url format
@@ -50,38 +38,82 @@ class XploreRequest(requests.Session):
         self.size = size
         self.co = colorizer.ColorOutput()
         self.wordlist = wl
+        
+        for code in gcode:
+            self.STATUS_DICT[code] = []
+        
+    def coprint(self, text):
+        # Extract status code from the string
+        match = re.search(r"\[(\d{3})\]", text)
+        if match:
+            status_code = match.group(1) 
 
+            if re.match(r"^2\d{2}$", status_code):
+                self.co.green.print(text)
+            elif re.match(r"^3\d{2}$", status_code):
+                self.co.blue.print(text)
+            elif re.match(r"^4\d{2}$", status_code):
+                self.co.red.print(text)
+            elif re.match(r"^5\d{2}$", status_code):
+                self.co.red.print(text)
+            else:
+                self.co.white.print(text)
+        else:
+            # Handle cases where the status code is not found
+            self.co.white.print(text)
+        
+    def make_request(self, wl_res):
+        try:
+            for line in wl_res.iter_lines(decode_unicode=True):
+                time.sleep(self.delay)
+                word = line.strip()
+                word = word[1:] if word.startswith('/') else word
+                url = f"{self.url}{word}"
+                res = self.get(url)
+                if res.status_code in self.gcode:
+                    self.coprint(f"[{res.status_code}] ")
+                    self.STATUS_DICT[res.status_code].append(url)
+                    print(url)
+        except ConnectionResetError:
+            self.co.red.printl("Connection reset by peer")
+            exit(1)
+        except KeyboardInterrupt:
+            print()
+            self.co.red.printl("Xplore terminated by user")
+            exit(0)
+            
+    def make_custom_request(self):
+        try:
+            for word in self.wordlist:
+                time.sleep(self.delay)
+                word = word[1:] if word.startswith('/') else word
+                url = f"{self.url}{word}"
+                res = self.get(url)
+                if res.status_code in self.gcode:
+                    self.coprint(f"[{res.status_code}] ")
+                    self.STATUS_DICT[res.status_code].append(url)
+                    print(url)
+        except ConnectionResetError:
+            self.co.red.printl("Connection reset by peer")
+            exit(1)
+        except KeyboardInterrupt:
+            print()
+            self.co.red.printl("Xplore terminated by user")
+            exit(0)
+            
+    def save_to_output(self):
+        with open(f"{self.output}.json", 'w') as jf:
+            json.dump(self.STATUS_DICT, jf)
+        
     def fuzz(self):
         if self.type != "custom":
             wl_url = self.WORDLISTS[self.type][self.size]
             with self.get(wl_url, stream=True) as wl_res:
                 wl_res.raise_for_status()
-                try:
-                    for line in wl_res.iter_lines(decode_unicode=True):
-                        time.sleep(self.delay)
-                        word = line.strip()
-                        word = word[1:] if word.startswith('/') else word
-                        url = f"{self.url}{word}"
-                        res = self.get(url)
-                        if res.status_code in self.gcode:
-                            coprint(f"[{res.status_code}] ")
-                            print(url)
-                except KeyboardInterrupt:
-                    print()
-                    self.co.red.printl("Xplore terminated by user")
-                    exit(0)
+                self.make_request(wl_res)
         else:
-            try:
-                for word in self.wordlist:
-                    time.sleep(self.delay)
-                    word = word[1:] if word.startswith('/') else word
-                    url = f"{self.url}{word}"
-                    res = self.get(url)
-                    if res.status_code in self.gcode:
-                        coprint(f"[{res.status_code}] ")
-                        print(url)
-            except KeyboardInterrupt:
-                    print()
-                    self.co.red.printl("Xplore terminated by user")
-                    exit(0)
+            self.make_custom_request()
+        
+        if self.output is not None:
+            self.save_to_output()
 
